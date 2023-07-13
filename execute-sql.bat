f3ac86ee6efa0159172f58854c88a462b9eb90ce
George Nairooze <george.nairooze@linkdev.com>
2023-07-13 12:44:13 +0300

    add sql authentication to the script

--- execute-sql.bat
+++ execute-sql.bat
@@ -1,6 +1,9 @@
 set servername=".\Dev"
-set dbname="Interview"
+set sqlusername="sample"
+set sqlpassword="P@ssw0rd"
+::by default sqlcmd uses windows authentication
+::use the arguments -U username -P password at the end of the sqlcmd command to use SQL authentication
 
 for %%f in (*.sql) do (
-    sqlcmd -S %servername% -d %dbname% -E -i "%%f"
+    sqlcmd -S %servername% -i "%%f" -U %sqlusername% -P %sqlpassword%
 )
--- sql-scripts/1.sql
+++ sql-scripts/1.sql
@@ -1,6 +1,9 @@
 use Interview
+go
 
 update Skill
 set Name = 'SQL-2',
     Modified_On = GETDATE()
 where Id = 3
+
+go
