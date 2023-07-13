8a864aa5cf8697a20583a88778f47abf937462da
George Nairooze <george.nairooze@linkdev.com>
2023-07-13 13:01:10 +0300

    add relative path

--- execute-sql.bat
+++ execute-sql.bat
@@ -1,9 +1,10 @@
+set source="sql-scripts\source\*.sql"
 set servername=".\Dev"
 set sqlusername="sample"
 set sqlpassword="P@ssw0rd"
 ::by default sqlcmd uses windows authentication
 ::use the arguments -U username -P password at the end of the sqlcmd command to use SQL authentication
 
-for %%f in (*.sql) do (
+for %%f in (%source%) do (
     sqlcmd -S %servername% -i "%%f" -U %sqlusername% -P %sqlpassword%
 )
