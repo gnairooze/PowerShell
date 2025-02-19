67c4a11f9a9a2dbc07c88bc9e93d2adbe1a4db78
george.nairooze <george.nairooze@gmail.com>
2025-02-19 11:01:25 +0200

    add progress bar

--- rename-files/rename-files-making-last-numbers-4-digits.ps1
+++ rename-files/rename-files-making-last-numbers-4-digits.ps1
@@ -1,10 +1,18 @@
-Get-ChildItem -Filter "*_*.tiff" | ForEach-Object {
+$files = Get-ChildItem -Filter "*_*.tiff"
+$total = $files.Count
+$i = 0
+
+$files | ForEach-Object {
+    $i++
+    Write-Progress -Activity "Processing Files" -Status "$i of $total" `
+        -PercentComplete (($i / $total) * 100)
+
     if ($_.BaseName -match '_(\d+)$') {
         $number = [int]$Matches[1]
         $paddedNumber = "{0:D4}" -f $number
         $suffix = "_$paddedNumber" + $_.Extension
         $newName = $_.BaseName -replace '_(\d+)$', $suffix
-        Write-Host "Renaming $($_.FullName) to $newName"
+        # Write-Host "Renaming $($_.FullName) to $newName"
 
         Rename-Item -Path $_.FullName -NewName $newName
     }
